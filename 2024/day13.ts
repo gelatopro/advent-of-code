1514b4fd9ce93d934d47211073ee375a79ac8566
gelatopro <sgympp@gmail.com>
2024-12-12 22:52:07 -0800

    day13 part2

--- 2024/day13.ts
+++ 2024/day13.ts
@@ -88,7 +88,78 @@ function calMinToken(
   return ans;
 }
 
+function calMinTokenV2(
+  a: Coordinates[],
+  b: Coordinates[],
+  prize: Coordinates[]
+): number {
+  let ans = 0;
+  for (let i = 0; i < a.length; i++) {
+    const minToken = _calc(
+      prize[i].x,
+      prize[i].y,
+      a[i].x,
+      a[i].y,
+      b[i].x,
+      b[i].y
+    );
+    if (minToken) ans += minToken;
+  }
+  return ans;
+}
+
+function _calc(
+  x: number,
+  y: number,
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number
+): number | null {
+  if (x1 * y2 === x2 * y1) {
+    // https://en.wikipedia.org/wiki/B%C3%A9zout%27s_identity
+    if (x1 * y2 === x2 * y1 && x2 * y === x * y2) {
+      const c = _gcd(y1, y2);
+      if (y % c === 0) {
+        // better solution?
+        let ans: number | null = null;
+        for (let i = 0; i < Math.floor(y / y1); i++) {
+          const tmp = y - y1 * i;
+          if (tmp >= 0 && tmp % y2 === 0) {
+            const j = tmp / y2;
+            if (!ans || i * 3 + j < ans) {
+              ans = i * 3 + j;
+            }
+          }
+        }
+        return ans;
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  } else {
+    const d = x1 * y2 - y1 * x2;
+    if ((x * y2 - y * x2) % d === 0 && (y * x1 - x * y1) % d === 0) {
+      const a = (x * y2 - y * x2) / d,
+        b = (y * x1 - x * y1) / d;
+      if (a >= 0 && b >= 0) {
+        return 3 * a + b;
+      } else {
+        return null;
+      }
+    }
+    return null;
+  }
+}
+
+function _gcd(a: number, b: number): number {
+  return b === 0 ? a : _gcd(b, a % b);
+}
+
 const { input } = _readFile();
 let { a, b, prize } = _processInput(input);
 console.log(calMinToken(a, b, prize));
 prize = _alterPrize(prize);
+console.log(calMinTokenV2(a, b, prize));
